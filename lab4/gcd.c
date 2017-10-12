87b5a64dd84711163a5157b9f210ee6a274554e1
joowon <saven7788@gmail.com>
2017-10-12 15:08:59 +0900

    lab4

---
+++ lab4/gcd.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int gcd(int a, int b)
+{
+  int c;
+  c = a+b;
+  return
+}
+
+int main(void)
+{
+  printf("gcd(10,15) = %d\n", gcd(10,15));
+  printf("gcd(125,13) = %d\n", gcd(125,13));
+  printf("gcd(625,225) = %d\n", gcd(625,225));
+  printf("gcd(6840,324) = %d\n", gcd(6840,324));
+
+}
