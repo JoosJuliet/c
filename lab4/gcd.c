7c321cb8ad9f4a3c31d3b0048335a09cec37660f
joowon <saven7788@gmail.com>
2017-10-17 00:40:37 +0900

    gcd refactoring

--- lab4/gcd.c
+++ lab4/gcd.c
@@ -1,20 +1,27 @@
 
 #include <stdio.h>
 
+int gcd(int, int);
+
+int main(void)
+{
+  printf("gcd(10,15) = %d\n", gcd(10,15));
+  printf("gcd(125,13) = %d\n", gcd(125,13));
+  printf("gcd(625,225) = %d\n", gcd(625,225));
+  printf("gcd(6840,324) = %d\n", gcd(6840,324));
+}
+
 int gcd(int a, int b)
 {
-
-int rem;
+  int rem;
   if(a<b)
-{
-	int tmp;
-
-tmp = a;
+  {
+  	int tmp;
+    tmp = a;
     a=b;
-	a = tmp;
-}
+  	a = tmp;
+  }
   // 더 큰수 a, 더 작은수 b
-
   rem = a%b; //깔끔히 나눠진다. 그렇다면 b가 최대공약수
   while(rem!=0) //나머지 있다 1
   {
@@ -23,13 +30,4 @@ tmp = a;
     rem =a%b;
   }
   return b;
-
-}
-int main(void)
-{
-  printf("gcd(10,15) = %d\n", gcd(10,15));
-  printf("gcd(125,13) = %d\n", gcd(125,13));
-  printf("gcd(625,225) = %d\n", gcd(625,225));
-  printf("gcd(6840,324) = %d\n", gcd(6840,324));
-
 }
