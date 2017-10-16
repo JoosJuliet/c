1e7209d6db825f0ec381cb4d047a53bc520f4111
deffjoowon <saven7788@gmail.com>
2017-10-17 00:35:59 +0900

    lab4 debugging

---
+++ lab4/compile-chap-4.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+gcc -o gcd gcd.c
+gcc -o fib fib.c
+gcc -o abs abs.c
+gcc -o switch switch.c
+
+./gcd
+./fib
+./abs
+./switch
+
--- lab4/gcd.c
+++ lab4/gcd.c
@@ -3,19 +3,28 @@
 
 int gcd(int a, int b)
 {
+
+int rem;
   if(a<b)
-     a = b;
-  int rem;
-  rem = a%b;
-  while(rem!=0)
+{
+	int tmp;
+
+tmp = a;
+    a=b;
+	a = tmp;
+}
+  // 더 큰수 a, 더 작은수 b
+
+  rem = a%b; //깔끔히 나눠진다. 그렇다면 b가 최대공약수
+  while(rem!=0) //나머지 있다 1
   {
     a=b;
-    b=rem;
+    b=rem; //공통의
     rem =a%b;
   }
-  return a;
-}
+  return b;
 
+}
 int main(void)
 {
   printf("gcd(10,15) = %d\n", gcd(10,15));
