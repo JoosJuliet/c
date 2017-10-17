2bad0ae5fec042b1aa58cbecd42ed652b73c7939
joowon <saven7788@gmail.com>
2017-10-17 18:08:30 +0900

    gcd 리팩토

--- lab4/gcd.c
+++ lab4/gcd.c
@@ -11,33 +11,35 @@ int main(void)
   printf("gcd(6840,324) = %d\n", gcd(6840,324));
 }
 
+/*선혁스 리팩토링
+int gcd(int a, int b)
+{
+  while( b!= 0 ){
+    int tmp = a;
+    a = b;
+    b = tmp%b;
+  }
+  return a;
+}
+*/
 int gcd(int big, int small)
 {
+  
   int tmp;
 
-/*  if(big<small)
+  if(big<small)
   {
-<<<<<<< HEAD
-    tmp = big; 
-	big= small;
+    tmp = big;
+    big = small;
   	small = tmp;
-=======
-    tmp=big;
-    big=small;
-  	small=tmp;
->>>>>>> d0edf6d118337c0aecc0447567418abd8f2ae493
   }
-*/
+
   // 더 큰수 big, 더 작은수 b
   tmp = big%small; //깔끔히 나눠진다. 그렇다면 small가 최대공약수
   while(tmp!=0) //나머지 있다 1
   {
     big=small;
-<<<<<<< HEAD
-small=tmp; //공통의
-=======
     small=tmp; //공통의
->>>>>>> d0edf6d118337c0aecc0447567418abd8f2ae493
     tmp=big%small;
   }
   return small;
