cc23df378aa093aa9b2afbe53e574f7c1a378887
joowon <saven7788@gmail.com>
2017-10-17 20:30:12 +0900

    two

--- chapter5/two.c
+++ chapter5/two.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include <math.h>
 
-int z;
-void f (int x)
-{
-  x=2;
-  z+=x;
-}
-int main(void)
-{
-  z=5;
-  f(z);
-  printf("z= %d\n",z);
+double k;
+int main(void) {
+
+  scanf("%f\n", &k);
+  k = sqrt(sqrt((double) k));
+  printf("%f\n", k);
+
   return 0;
 }
+
+/*
+  Use the library function sqrt() to write a function that returns the fourth root of its int argument k.
+  The value returned should be a double.
+  Use your function to make a table of values
+
+
+
+*/
