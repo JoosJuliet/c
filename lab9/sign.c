e7d2e52492c145a7b0827a1b0114d65ade21822a
deffjoowon <saven7788@gmail.com>
2017-11-16 17:14:53 +0900

    error fix

--- lab9/sign.c
+++ lab9/sign.c
@@ -20,26 +20,24 @@ void print_sign(int a)
 
 void moving_sign(char _a, char _b, char _c, char _d)
 {
+
   char a = _a, b = _b, c = _c, d = _d, tmp;
   int p;
-  p = _a;
-  p = (p << CHAR_BIT) | _b;
-  p = (p << CHAR_BIT) | _c;
-  p = (p << CHAR_BIT) | _d;
+  p = a;
+  p = (p << CHAR_BIT) | b;
+  p = (p << CHAR_BIT) | c;
+  p = (p << CHAR_BIT) | d;
 
-//  while(1)
- // {
+while(1){
     system("clear");
     print_sign(p);
     sleep(1);
-<<<<<<< HEAD
- // }
-=======
-    tmp = _a;
-    _b << p
-    _c <<p << p 
-    _d <<p << p  << p
+
+    tmp = p >> 3* CHAR_BIT;
+    p = p << 1*CHAR_BIT;
+    p |= tmp;
+
 
   }
->>>>>>> ecf807764f9f5e7cb6e7fb067cbe5f2c84def275
+
 }
