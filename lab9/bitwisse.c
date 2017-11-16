eb83892fd062d1eda7fd62cbe098785140084bb4
joowon <saven7788@gmail.com>
2017-11-16 15:59:16 +0900

    빼놓은 코드 넣기

--- lab9/bitwisse.c
+++ lab9/bitwisse.c
@@ -7,6 +7,42 @@ void bit_print(int);
 int pack(char,char,char,char);
 char unpack(int,int);
 
+void bit_print(int a)
+{
+  int i;
+  int n = sizeof(int) * CHAR_BIT;
+  int mask = 1 << (n-1);
+
+  for (i = 1; i <= n; ++i) {
+    putchar(((a&mask) == 0) ? '0' : '1');
+    a <<=1;
+    if( i % CHAR_BIT == 0 && i <n)
+      putchar('  ');
+        /* code */
+  }
+  putchar('\n');
+}
+
+int pack(char a, char b, char c, char d)
+{
+  int p;
+  p = a;
+  p = (p << CHAR_BIT) | b;
+  p = (p << CHAR_BIT) | c;
+  p = (p << CHAR_BIT) | d;
+  return p;
+}
+
+char unpack (int p,int k)
+{
+  int n = k* CHAR_BIT;
+  unsigned mask = 255;
+
+  mask <<=n;
+  return ((p & mask) >> n);
+}
+
+
 int main(int argc, char *argv[])
 {
   if(argc <3)
