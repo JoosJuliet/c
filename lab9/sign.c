0fab7c6b84b417f8ab186703f5f3c664cb8b97ba
joowon <saven7788@gmail.com>
2017-11-16 15:50:29 +0900

    lab9 올리기

---
+++ lab9/sign.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <unistd.h>
+
+void moving_sign(char, char, char, char);
+void print_sign(int);
+
+int main(void)
+{
+  moving_sign('H','E','Y',' ');
+  return 0;
+}
+
+void print_sign(int a)
+{
+  printf("%c %c %c %c\n",
+  a/(256*256*256),a/(256*256),a/256,a);
+}
+
+void moving_sign(char _a, char _b, char _c, char _d)
+{
+  char a = _a, b = _b, c = _c, d = _d, tmp;
+  int p;
+
+  while(1)
+  {
+    system("clear");
+    print_sign(p);
+    sleep(1);
+  }
+}
