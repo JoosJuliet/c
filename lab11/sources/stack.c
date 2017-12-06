996e453effd9e5347c3b6c0e6c9e09ef015086ea
joowon <saven7788@gmail.com>
2017-12-06 19:19:45 +0900

    stack upgrade

--- lab11/sources/stack.c
+++ lab11/sources/stack.c
@@ -5,25 +5,84 @@
 /* Define 'stack' using typedef and struct.
 stack has top, size */
 
-stack* init_queue();
-void enqueue(queue* node*);
-void dequeue(queue*);
-int front(queue*)
-void print_queue(queue*);
+typedef struct stack {
+	node *top;
+	int size;
+} stack;
+
+stack* init_stack();
+void push(stack* node*);
+int pop(stack*);
+int front(stack*)
+void print_stack(stack*);
 
 int main(int argc, char const *argv[]) {
   int i;
-  queue* q = init_queue();
-  enqueue(q, newnode(1));
-  printf("front : %d\n", front(q));
-  dequeue(q);
-  dequeue(q);
+  stack* s = init_stack();
+  push(s, newnode(1));
+  printf("top : %d\n", top(s));
+  pop(s);
+  pop(s);
   for ( i = 1; i < 6; i++) {
-    enqueue(q,newnode(i));
+    push(s,newnode(i));
   }
-  dequeue(q);
-  print_queue(q);
-  for(i=0; i<3; i++) dequeue(q);
-  front(q);
+  pop(s);
+  print_stack(s);
+  for(i=0; i<3; i++) pop(s);
+  top(s);
   return 0;
+}
+
+stack* init_stack(){
+	stack* temp = (stack*)malloc(sizeof(stack));
+	temp->top=NULL;
+	temp->size = 0;
+	return temp;
+}
+void push(stack* stack, node* newnode){
+	if(stack->size==0){
+		stack->top=newnode;
+		stack->size++;
+		return ;
+	}
+	else{
+		newnode->next = stack->top;
+		stack->top=newnode;
+		stack->size++;
+	}
+}
+void pop(stack* stack){
+	if(stack->size<1){
+		printf("pop() : stack is empty\n");
+		return;
+	}
+	if(stack->size==1){
+		node* tmp =stack->top;
+		stack->size--;
+		free(tmp);
+		return ;
+	}
+
+	node* tmp = stack->top;
+	stack->top = stack->top->next;
+	stack->size--;
+	free(tmp);
+}
+int top(stack* stack){
+	if(stack->size<1){
+		printf("top() : stack is empty\n");
+		return -1;
+	}
+
+	return stack->top->val;
+}
+void print_stack(stack* stack){
+	node* cur = stack->top;
+
+	printf("stack size : %d \n",stack->size);
+	printf("top\n");
+	for(int i=0;i<stack->size;i++){
+		printf("[%d]\n",cur->val);
+		cur=cur->next;
+	}
 }
