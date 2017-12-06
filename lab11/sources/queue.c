bd7cadeb1316d4173a402186c78db8dd22d3ae6a
joowon <saven7788@gmail.com>
2017-12-06 19:30:00 +0900

    queue.c complete

--- lab11/sources/queue.c
+++ lab11/sources/queue.c
@@ -4,6 +4,11 @@
 
 /* Define 'queue' using typedef and struct.
 queue has front, rear, and size */
+typedef struct queue{
+		node* front;
+		node* rear;
+		int size;
+	}queue;
 
 queue* init_queue();
 void enqueue(queue* node*);
@@ -26,4 +31,53 @@ int main(int argc, char const *argv[]) {
   for(i=0; i<3; i++) dequeue(q);
   front(q);
   return 0;
+}
+queue* init_queue(){
+	queue* temp = (queue*)malloc(sizeof(queue));
+	temp->front=NULL;
+	temp->rear =NULL;
+	temp->size = 0;
+	return temp;
+}
+void enqueue(queue* que, node* newnode){
+	if(que->size==0){
+		que->front=newnode;
+		que->rear=newnode;
+		que->front->next = newnode;
+		que->size++;
+		return ;
+	}
+	else{
+		que->rear->next = newnode;
+		que->rear = que->rear->next;
+		que->size++;
+	}
+}
+void dequeue(queue* que){
+	if(que->size<1){
+		printf("deque() :queue is empty\n");
+		return ;
+	}
+	else{
+		node* tmp = que->front;
+		que->front = que->front->next;
+		que->size--;
+		free(tmp);
+	}
+}
+int front(queue* que){
+	if(que->size<1){
+		printf("front() :queue is empty\n");
+		return -1;
+	}
+	return que->front->val;
+}
+void print_queue(queue* que){
+	node* cur = que->front;
+	printf("Q : ");
+	for(int i=0;i<que->size;i++){
+		printf("[%d]",cur->val);
+		cur=cur->next;
+	}
+	putchar('\n');
 }
