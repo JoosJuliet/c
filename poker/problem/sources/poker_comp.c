54e0eea1dd577c5a558e1047627d0aebd70febe0
joowon <saven7788@gmail.com>
2017-12-07 15:34:20 +0900

    test

--- poker/problem/sources/poker_comp.c
+++ poker/problem/sources/poker_comp.c
@@ -1,37 +1,44 @@
 #include "../headers/poker.h"
 
-/* [ Chapter 9: Structures and Unions ]  
+/* [ Chapter 9: Structures and Unions ]
  *
  *  Functions to create
- *  
+ *
  *	1) newcard(-,-): Use malloc
  *
- *	2) newdeck(): Use malloc 
- *	
+ *	2) newdeck(): Use malloc
+ *
  * 	3) newplayer(): Use malloc
  *
- *  	4) eject(): only can pop out from the top of the Deck 
- *		    (pop from the bottom is not allowed) 
+ *  	4) eject(): only can pop out from the top of the Deck
+ *		    (pop from the bottom is not allowed)
  *
  *	5) shuffle(-): use srand/rand
  */
 
 /* function to complete */
 Card* newcard(int _pips,char _suit){
-	
-	/* write codes */	
+	Card* card = (Card*)malloc(sizeof(Card));
+	card->pips = _pips;
+	card->suit = _suit;
+	card->flipped = 0;
+	card->next = NULL;
+	/* write codes */
 
-	return card;	
+	return card;
 }
-	
-Deck* newdeck(){
-	
-	/* write codes */	
 
-	Card* cursor;	
+Deck* newdeck(){
+
+	Deck* deck = (Deck*)malloc(sizeof(Deck));
+	deck->top=NULL;
+	deck->size=0;
+	/* write codes */
+
+	Card* cursor;
 	for(char s = 0; s <= 3; s++) {
 		for(int p = 2; p <= 14; p++) {
-			if(deck->top == NULL) { 
+			if(deck->top == NULL) {
 				deck->top = newcard(p,s);
 				cursor = deck->top;
 			} else {
@@ -44,16 +51,26 @@ Deck* newdeck(){
 }
 
 Player* newplayer(char* _name,int _account){
-	
-	/* write codes */	
-	
+	Player* player = (Player*)malloc(sizeof(Player));
+	player->name = _name;
+	player->c1 = NULL;
+	player->c2 = NULL;
+	player->account = _account;
+	/* write codes */
+
 	return player;
 }
 
 Card* eject(Deck* deck) {
 	Card* temp;
 
-	/* write codes */	
+	/* write codes */
+	if(deck->top == NULL){
+		printf("error failed!\n");
+		return temp;
+	}
+	deck->top = deck->top->next;
+	deck->size--;
 
 	return temp;
 }
@@ -64,11 +81,11 @@ void shuffle(Deck* deck){
 	Deck* tail = (Deck*)malloc(sizeof(Deck));
 
 	srand(clock()); 	/* time(NULL) 보다 훨씬 빠른 주기로 시드를 변경한다. 프로그램이 실행된 이후 얼만큼의 cpu clock tick이 경과했는지 측정한다. */
-	
+
 	if(deck->size<=20) printf("\n\nERROR: deck size should be bigger than 20\n\n");
 	unsigned char index = rand()%(deck->size-20)+10;
 	unsigned char endex = rand()%(deck->size - index-1)+1;
-	
+
 	Card *cindex, *cendex, *aendex, *bendex;
 	cendex = deck->top;
 	for(int i = 0; i < index-1; i++) {
@@ -81,7 +98,7 @@ void shuffle(Deck* deck){
 	cendex->next = NULL;
 	aendex = cendex;
 	cendex = cindex;
-	
+
 	for(int i = 0; i < endex-1; i++) {
 		cendex = cendex->next;
 	}
@@ -98,22 +115,22 @@ void shuffle(Deck* deck){
 		cendex = cendex->next;
 	}
 	cendex->next = NULL;
-	
+
 	/* 테일 덱이 생성된다. */
 	tail->top = cindex;
-	tail->size = (deck->size - index - endex);	
-	
+	tail->size = (deck->size - index - endex);
+
 	deck->top = mid->top;
 	aendex->next = tail->top;
 	bendex->next = head->top;
 
-	free(head); free(mid); free(tail); 		
+	free(head); free(mid); free(tail);
 	return;
 }
 
 void freedeck(Deck* deck){  	/* malloc을 해줬던 메모리 영역들에 대하여 free()를 하지 않으면 메모리 문제가 발생하여 런타임 에러가 날 수 있다. */
 
-	/* write codes */	
+	/* write codes */
 
 	return;
 }
